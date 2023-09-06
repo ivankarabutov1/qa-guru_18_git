2af6501c3a9e58d90add8837e1ac9b65a092129a
ivankarabutov1 <74875462+ivankarabutov1@users.noreply.github.com>
2023-09-06 16:30:24 +0300

    Update Main.java

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -4,10 +4,6 @@ package guru.qa;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome! this is I!!!!!!!");
-
-        
     }
 }
